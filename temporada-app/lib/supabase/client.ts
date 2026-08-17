fc6bce81f02499ad2aab10fa0946f79d6733e999
progtitos <47480315+progtitos@users.noreply.github.com>
2026-08-17 16:37:17 -0300

    Update client.ts

--- temporada-app/lib/supabase/client.ts
+++ temporada-app/lib/supabase/client.ts
@@ -3,6 +3,6 @@ import { createBrowserClient } from '@supabase/ssr'
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://stpivxmptsxabdpsimlb.supabase.co',
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0cGl2eG1wdHN4YWJkcHNpbWxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3MzE0NTcsImV4cCI6MjEwMjMwNzQ1N30.K28WtxPkpWYvaN3cTV0b2SH_flDFuUxCJw0tnrxMVdM'
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
